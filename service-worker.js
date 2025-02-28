2867ac7a8f6739e36b0b1d4e31c535e38809b079
HangJUdlqslek <20241107@hanilgo.cnehs.kr>
2025-02-28 16:07:53 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,16 +5,9 @@ self.addEventListener('install', (event) => {
         '/',
         '/index.html',
         '/manifest.json',
-        '/icon.png',
+        'Tools/icon-192.png',
+        'Tools/icon-512.png'
       ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
     })
   );
 });
