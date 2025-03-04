56780d0bd53bde7a23004633321b36988f8e17f7
Hanil_MOI <20241107@hanilgo.cnehs.kr>
2025-03-05 08:41:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
+    caches.open('my-cache').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
