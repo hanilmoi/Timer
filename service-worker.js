6b6e121110d1fd3b0d653cdce750a554cee04ba5
Hanil_MOI <20241107@hanilgo.cnehs.kr>
2025-03-06 08:10:10 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,37 @@
+const CACHE_NAME = 'my-cache';
+
+const CACHE_ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    'Tools/icon-192.png',
+    'Tools/icon-512.png'
+];
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('my-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        'Tools/icon-192.png',
-        'Tools/icon-512.png'
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(CACHE_ASSETS);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match('/index.html'))
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || new Response('No cache in resource', { status: 404 });
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cache) => cache !== CACHE_NAME)
+                    .map((cache) => caches.delete(cache))
+            );
+        })
+    );
 });
